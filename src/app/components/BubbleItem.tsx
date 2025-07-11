7128c6f032bee448dedbd1083925a4fc4ff12036
ryu <yongjingliu7@gmail.com>
2025-07-11 14:38:04 +0900

    responsive only for PC

--- src/app/components/BubbleItem.tsx
+++ src/app/components/BubbleItem.tsx
@@ -17,13 +17,12 @@ export default function BubbleItem({ iconSrc, alt, label, iconComponent, onClick
   const handleHover = () => {
     if (isAnimating) return;
     setIsAnimating(true);
-
-    setAnimationStyle({ animation: "bubble-pop 0.6s ease-out" });
+    setAnimationStyle({ animation: "bubble-pop 0.6s ease-in-out forwards" });
 
     setTimeout(() => {
       setAnimationStyle({});
       setIsAnimating(false);
-    }, 500);
+    }, 450);
   };
 
   const altText =
@@ -35,22 +34,53 @@ export default function BubbleItem({ iconSrc, alt, label, iconComponent, onClick
 
   return (
     <div
-      onMouseEnter={handleHover}
-      onClick={onClick}
-      style={animationStyle}
-      className="flex flex-col items-center justify-center w-[115px] p-2 mr-[5px] bubble-container cursor-pointer"
-    >
-      {iconComponent && iconComponent}
+  onMouseEnter={handleHover}
+  onClick={onClick}
+  style={animationStyle}
+  className="
+    flex flex-col items-center justify-center
+    bubble-container cursor-pointer
+    transition-all duration-300
+  "
+>
+     {iconComponent && (
+        <div
+          className="
+            flex items-center justify-center
+          "
+          style={{
+            width: "clamp(20px, 4vw, 70px)",
+            height: "clamp(20px, 4vw, 70px)",
+          }}
+        >
+          {iconComponent}
+        </div>
+      )}
       {iconSrc && (
         <Image
           src={iconSrc}
           alt={altText}
-          width={60}
-          height={60}
+          width={42}
+          height={42}
+          className="
+          "
+          style={{
+            width: "clamp(20px, 4vw, 70px)",
+            height: "clamp(20px, 4vw, 70px)",
+          }}
         />
       )}
       {label && (
-        <p className="text-[12px] text-center mt-1">{label}</p>
+        <p
+          className="
+          "
+          style={{
+            fontSize:"clamp(5.5px, 0.8vw, 20px)", 
+            
+          }}
+        >
+          {label}
+        </p>
       )}
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { FaGithub, FaXTwitter} from "react-icons/fa6";
 import BubbleCanvas from "./components/bubbles";
-import { SiHtml5,SiCss3,SiJavascript,SiTypescript,SiPython } from "react-icons/si";
+import { SiHtml5,SiCss3,SiJavascript,SiTypescript,SiPython, } from "react-icons/si";
 import BubbleItem from "./components/BubbleItem";
 import { useState } from "react";
 
@@ -26,61 +26,80 @@ export default function Home() {
     </div>
       
       <main className="relative flex flex-col z-20 h-full">
+
 {/* about */}
-        <div className = "flex flex-row justify-center mt-7"> 
+        <div className = "flex flex-row justify-center mt-[4%] gap-x-[5%]"> 
+          <div 
+            className="relative w-[60px] h-[80px]
+            sm:w-[60px] sm:h-[80px]
+            md:w-[81px] md:h-[108px]
+            lg:w-[90px] lg:h-[120px]"
+            
+          >
           <Image
-            className=""
+            className="object-cover"
             src={"/my-picture.jpg"}
             alt="my-picture"
-            width={120}
-            height={160}          
+            fill         
           />
+          </div>
 
-          <div>
-          <h1 className="text-base sm:text-lg md:text-xl lg:text-3xl">
-          永井琉
-          </h1>
-          <p>
-            豊橋技術科学大学工学部情報・知能工学科2年(2028年3月卒業予定)
-          </p>
-          <p>
-            web,アプリ開発のバックエンド、フロントエンド共に関心を持っています。
-          </p>
-          <p>
-            見やすく使いやすい設計を考えることが好きです。
-          </p>
+          <div className="w-[50%]">
+            <h1 
+              className=""
+              style={{ fontSize:"clamp(10px,4.5vw,30px) "}}>
+              永井琉
+            </h1>
+            <div 
+              className=""
+              style={{ fontSize:"clamp(9px,2vw,17px) "}}>
+              <p className="mb-[2%]">
+                豊橋技術科学大学工学部情報・知能工学科2年(2028年3月卒業予定)
+              </p>
+              <p>
+                web,アプリ開発のバックエンド、フロントエンド共に関心を持っています。
+              </p>
+              <p>
+                見やすく使いやすい設計を考えることが好きです。
+              </p>
+            </div>
           </div>
           
-          <div className="">
-            <a 
+          <div className="flex flex-col items-center gap-y-4 w-[10%] min-w-15 max-w-27">
+            <a
               href="https://github.com/Ryu-0402"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="GitHub"
+              className="w-[70%] "
             >
-              <FaGithub size={60} />
+            <FaGithub className="w-full h-auto text-white      hover:opacity-80 transition" />
             </a>
-            <a 
+
+            <a
               href="https://x.com/RyuN_0402?s=21"
               target="_blank"
               rel="noopener noreferrer"
-              aria-label="GitHub"
+              aria-label="X"
+              className="w-[70%]"
             >
-              <FaXTwitter size={60} />
+            <FaXTwitter className="w-full h-auto text-white hover:opacity-80 transition" />
             </a>
           </div>
         </div>
 {/* about end */}
 
 {/* skills,projects */}
-        <div className="relative flex flex-1 mt-10">
+        <div className="relative flex flex-1 mt-[4%]">
     {/* skills */}
-          <div className="flex flex-col ml-[10px] mr-auto">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl">
+          <div className="flex flex-col ml-[0.5%] mr-auto w-[30%]">
+            <h1 
+              className="text-center"
+              style={{ fontSize:"clamp(10px,4.5vw,45px) "}}>
               Skills
             </h1>
 
-            <div className="grid grid-cols-3 mt-5 gap-y-3">
+            <div className="grid grid-cols-3 mt-[3%] gap-y-[9%]">
               <BubbleItem 
                 iconComponent={<SiHtml5 size={60}
                 className="text-[#e34c26]" />}
@@ -109,7 +128,7 @@ export default function Home() {
               />
             </div>
 
-            <div className="flex flex-row mt-3">
+            <div className="grid grid-cols-3 mt-[10%] gap-y-[9%]">
               <BubbleItem
                 label={<>基本情報<br/>技術者試験</>}
                 alt="基本情報技術者試験"
@@ -126,16 +145,22 @@ export default function Home() {
 
 
     {/* details */}
-          <div className="absolute left-1/2 transform -translate-x-1/2 flex flex-col items-center  w-[35%] h-[80%] ">
+          <div 
+            className="absolute left-1/2 transform -translate-x-1/2 flex flex-col items-center  w-[35%] h-[80%]"
+            style={{fontSize:"clamp(10px,vw,20px) "}}
+          >
               {selectedBubble === null &&(
-                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl text-center mt-10">
+                <h1 className="text-center"
+                  style={{ fontSize:"clamp(10px,4.5vw,45px) "}}>
                   Please click bubbles to see details
                 </h1>)
               }
 
               {selectedBubble === "HTML" && (
                 <div className="flex flex-col ">
-                  <h2 className="text-xl text-center">HTML</h2>
+                  <h2 className=" text-center
+                  mb-[5%]"
+                  style={{fontSize:"clamp(25px,4vw,50px)"}}>HTML</h2>
                   <p>HTMLはウェブページの構造を定義するためのマークアップ言語です。</p>
                   <p>基本的なタグや属性を理解し、セマンティックなマークアップを心がけています。</p>
                 </div>
@@ -143,7 +168,8 @@ export default function Home() {
               
               { selectedBubble === "CSS" && (
                 <div className="flex flex-col ">
-                  <h2 className="text-xl text-center">CSS</h2>
+                  <h2 className=" text-center mb-[5%]"
+                  style={{fontSize:"clamp(25px,4vw,50px)"}}>CSS</h2>
                   <p>CSSはウェブページのスタイルを定義するためのスタイルシート言語です。</p>
                   <p>レイアウト、色、フォントなどを調整し、レスポンシブデザインにも対応しています。</p>
                 </div>
@@ -151,7 +177,8 @@ export default function Home() {
 
               { selectedBubble === "JavaScript" && (
                 <div className="flex flex-col ">
-                  <h2 className="text-xl text-center">JavaScript</h2>
+                  <h2 className=" text-center mb-[5%]"
+                  style={{fontSize:"clamp(25px,4vw,50px)"}}>JavaScript</h2>
                   <p>JavaScriptはウェブページに動的な機能を追加するためのプログラミング言語です。</p>
                   <p>DOM操作やイベント処理、非同期通信などを利用して、インタラクティブなウェブアプリケーションを作成します。</p>
                 </div>
@@ -159,7 +186,8 @@ export default function Home() {
 
               { selectedBubble === "TypeScript" && (
                 <div className="flex flex-col ">
-                  <h2 className="text-xl text-center">TypeScript</h2>
+                  <h2 className=" text-center mb-[5%]"
+                  style={{fontSize:"clamp(25px,4vw,50px)"}}>TypeScript</h2>
                   <p>TypeScriptはJavaScriptのスーパーセットで、型安全性を提供します。</p>
                   <p>大規模なアプリケーション開発において、コードの可読性と保守性を向上させるために使用しています。</p>
                 </div>
@@ -167,7 +195,8 @@ export default function Home() {
 
               { selectedBubble === "Python" && (
                 <div className="flex flex-col ">
-                  <h2 className="text-xl text-center">Python</h2>
+                  <h2 className=" text-center mb-[5%]"
+                  style={{fontSize:"clamp(25px,4vw,50px)"}}>Python</h2>
                   <p>Pythonは汎用的なプログラミング言語で、データ分析や機械学習など幅広い分野で使用されています。</p>
                   <p>簡潔な構文と豊富なライブラリが特徴で、バックエンド開発にも利用しています。</p>
                 </div>
@@ -175,7 +204,8 @@ export default function Home() {
 
               { selectedBubble === "基本情報技術者試験" && (
                 <div className="flex flex-col ">
-                  <h2 className="text-xl text-center">基本情報技術者試験</h2>
+                  <h2 className=" text-center mb-[5%]"
+                  style={{fontSize:"clamp(22px,3.5vw,50px)"}}>基本情報技術者試験</h2>
                   <p>基本情報技術者試験は、ITエンジニアの基礎的な知識と技能を評価する試験です。</p>
                   <p>プログラミング、ネットワーク、データベースなどの幅広い分野にわたる知識が求められます。</p>
                 </div>
@@ -183,7 +213,8 @@ export default function Home() {
 
               { selectedBubble === "toeic" && (
                 <div className="flex flex-col ">
-                  <h2 className="text-xl text-center">TOEIC</h2>
+                  <h2 className=" text-center mb-[5%]"
+                  style={{fontSize:"clamp(25px,4vw,50px)"}}>TOEIC</h2>
                   <p>TOEICは英語のコミュニケーション能力を測定する試験です。</p>
                   <p>リスニングとリーディングのスキルを評価し、国際的なビジネス環境での英語力を証明します。</p>
                 </div>
@@ -191,7 +222,8 @@ export default function Home() {
 
               { selectedBubble === "Photoタイマー" && (
                 <div className="flex flex-col ">
-                  <h2 className="text-xl text-center">Photoタイマー</h2>
+                  <h2 className=" text-center mb-[5%]"
+                  style={{fontSize:"clamp(25px,4vw,50px)"}}>Photoタイマー</h2>
                   <p>Photoタイマーは、写真撮影のタイマー機能を提供するアプリケーションです。</p>
                   <p>ユーザーが指定した時間に写真を自動で撮影できるようにします。</p>
                   <p>ReactとTypeScriptを使用して開発され、ユーザーインターフェースはシンプルで直感的です。</p>
@@ -200,7 +232,8 @@ export default function Home() {
 
               { selectedBubble === "ポートフォリオ" && (
                 <div className="flex flex-col ">
-                  <h2 className="text-xl text-center">ポートフォリオ</h2>
+                  <h2 className="text-center mb-[5%]"
+                  style={{fontSize:"clamp(25px,4vw,50px)"}}>ポートフォリオ</h2>
                   <p>このポートフォリオは、私のスキルとプロジェクトを紹介するために作成されました。</p>
                   <p>Next.jsとTailwind CSSを使用して、レスポンシブで魅力的なデザインを実現しています。</p>
                 </div>
@@ -211,8 +244,12 @@ export default function Home() {
 
 
     {/* projects */}
-          <div className="flex flex-col w-[365px]">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl">
+          <div className="flex flex-col w-[30%]">
+            <h1 
+              className="text-center"
+              style={{ fontSize:"clamp(10px,4.5vw,45px) "}}
+            >
+
               Projects
             </h1>
             <div className="grid grid-cols-3 mt-5">
